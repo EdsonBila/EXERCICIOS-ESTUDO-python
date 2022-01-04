2222bfc1633b23fa84a8ea0fbeb91a6afab0db29
Edson Bila dos Santos Junior <88840886+EdsonBila@users.noreply.github.com>
2022-01-04 01:07:34 -0300

    codigo feito

---
+++ 1-Estrutura_Sequencial/10.py
@@ -0,0 +1,5 @@
+'''Faça um programa que calcule e mostre a área de um círculo. Sabe-se que: Área = pi * R 2 (este 2 signica elevado ao quadrado) e o valor da variável pi é 3.1415.'''
+print('Cálculo da área de um círculo')
+raio = float(input('Digite o raio: '))
+area = 3.1415 * raio ** 2
+print('Área do círculo: ',area)
