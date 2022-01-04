eea01bd5f6f2d8b82eb6f422dae020e926a619d1
Edson Bila dos Santos Junior <88840886+EdsonBila@users.noreply.github.com>
2022-01-04 01:01:58 -0300

    codigo feito

---
+++ 1-Estrutura_Sequencial/3.py
@@ -0,0 +1,8 @@
+'''Faça um programa que receba três notas, calcule e mostre a média aritmética.'''
+texto = str('Digite as notas ')
+print('',texto)
+nota1 = float(input('Nota 1: '))
+nota2 = float(input('Nota 2: '))
+nota3 = float(input('Nota 3: '))
+media = (nota1 + nota2 + nota3) / 3
+print('Media: ',media)
